cd461d85e32b30e00470bfef85e9206de0e68f04
Gayathri K <60387180+Gayathri-K-2000@users.noreply.github.com>
2022-06-24 09:55:07 +0530

    final changes

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
     #path('done.html', TemplateView.as_view(template_name='done.html')),
     path('done/', done),
+
 ]
--- django_project/django_project/views.py
+++ django_project/django_project/views.py
@@ -1,13 +1,20 @@
 # import Http Response from django
 from django.http import HttpResponse
+import pyodbc 
+from .forms import NameForm
 
- 
+dbcon = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};Server=localhost;Database=django;Trusted_Connection=yes;')
+cursorObject= dbcon.cursor()
+    
 # create a function
 def done(request):
     print(request)
-  
+    form = NameForm()
     value1= request.GET.get('first_val')
     value2= request.GET.get('second_val')
     value3= request.GET.get('third_val')
-
-    return HttpResponse("Here is the response from user: " +value1+ " , "+value2+ " , "+value3)
+    #if form.is_valid():
+    cursorObject.execute("INSERT INTO [dbo].[Prj01] ([How are you],	[Where are you], [Who are you]) VALUES (?,?,?);", (value1,value2,value3))
+    dbcon.commit()
+    dbcon.close()
+    return HttpResponse("Values stored in database!  Here is the response from user: " +value1+ " , "+value2+ " , "+value3)
